2fd44ba468c0a22317c9721ab0fb80c39bc5d964
Simon Burema <simonburema@MacBook-Pro-van-Simon.local>
2020-06-17 21:36:47 +0200

    removed some code

--- test.py
+++ test.py
@@ -4,12 +4,6 @@ import sys
 
 import requests
 
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
 r = requests.get("https://google.com")
 print(r.status_code)
 
