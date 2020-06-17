1a9fa5fb15398b44e00b5d44484a5b5266b8f869
Simon Burema <simonburema@MacBook-Pro-van-Simon.local>
2020-06-17 21:56:47 +0200

    Snake part 1

---
+++ snake.py
@@ -0,0 +1,16 @@
+# snake game
+
+import os
+import turtle
+import time
+import random
+
+# setup the screen
+window = turtle.Screen()
+window.title("PythonSnake")
+window.bgcolor("black")
+window.setup(width=600, height=600)
+window.tracer(0)  # turns off animation on the screen
+
+
+window.mainloop()
