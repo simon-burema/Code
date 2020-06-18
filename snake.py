def525a0d1cdc62e189f203be097f894ebc70969
Simon Burema <simonburema@MacBook-Pro-van-Simon.local>
2020-06-18 13:54:44 +0200

    Snake game final

--- snake.py
+++ snake.py
@@ -7,6 +7,10 @@ import random
 
 delay = 0.1
 
+# Score
+score = 0
+high_score = 0
+
 # setup the screen
 window = turtle.Screen()
 window.title("PythonSnake")
@@ -31,18 +35,35 @@ food.color("yellow")
 food.penup()
 food.goto(0, 100)
 
+segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score:  0 High Score:  0", align="center", font=("Courier", 24, "normal"))
+
+
 # Functions
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 def move():
     if head.direction == "up":
@@ -69,14 +90,82 @@ window.onkeypress(go_left, "Left")
 # Main game loop
 while True:
     window.update()
+
+    # Check for collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+        # Hide segments after death
+        for segment in segments:
+            segment.goto(1000,1000)
+        # Clear segment list
+        segments.clear()
+
+        # Score
+        score = 0
+        pen.clear()
+        pen.write("Score:  {} High Score:  {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+    
+    #check for collision with the food
     if head.distance(food) < 20:
         # move the food to a random spot on the screen
-        x = random.randint(-290, 290)
-        y = random.randint(-290, 290)
+        x = random.randrange(-280, 280, 20)
+        y = random.randrange(-280, 280, 20)
         food.goto(x,y)
+
+        #add segment to Snake
+        new_segment =  turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("orange")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        
+        pen.clear()
+        pen.write("Score:  {} High Score:  {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments) -1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x,y)   
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
 
 
     move()
+
+    # Check for head collision with the body
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            # Hide segments after death
+            for segment in segments:
+                segment.goto(1000,1000)
+
+            # Clear segment list
+            segments.clear()
+
+            # Score
+            score = 0
+            pen.clear()
+            pen.write("Score:  {} High Score:  {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+
     time.sleep(delay)
 
 window.mainloop()
